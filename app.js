ce9a5e79df66962d5209378a92875a42c600e68d
Anna K <akuroshenkova@gmail.com>
2023-06-08 16:05:00 +0100

    working version

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ let userName = prompt('Hi! What is your name, please?');
 console.log(userName);
 //confirm("Are you 100% sure your name is " + userName + '?');
 while (confirm("Are you 100% sure your name is "+ userName + '?') == false) {
-    let userName = prompt('Hi! What is your name, please?');
+    userName = prompt('Hi! What is your name, please?');
     
 }
-userName = prompt('Hi! What is your name, please?')
+
 let welMsg = alert('Welcome to this page, ' + userName);
 
-console.log(userName);
+document.write(userName);
 let userColor = prompt('Give me a color!')
 document.body.style.backgroundColor = userColor
 if (userColor === 'yellow') {

48a17a751b22f9aedb6f23d1c696415f9d5a4f5a
Anna K <akuroshenkova@gmail.com>
2023-06-09 12:30:40 +0100

    Function calling another function

--- app.js
+++ app.js
@@ -3,19 +3,34 @@ console.log(myName);
 console.log(typeof myName);
 let userName = prompt('Hi! What is your name, please?');
 console.log(userName);
-//confirm("Are you 100% sure your name is " + userName + '?');
+
 while (confirm("Are you 100% sure your name is "+ userName + '?') == false) {
     userName = prompt('Hi! What is your name, please?');
     
 }
 
-let welMsg = alert('Welcome to this page, ' + userName);
+let welMsg = alert('Welcome to this page, ' + userName + '!');
 
 document.write(userName);
-let userColor = prompt('Give me a color!')
-document.body.style.backgroundColor = userColor
-if (userColor === 'yellow') {
-    document.write(' Yellow is my favourite color too!');
-} else {
-    document.write(' ')
+
+function extraMsg(userColor){
+    let msg = ', Yellow is my favourite color too!'
+    if (userColor == 'yellow') {
+        return document.write(msg)
+    }
+    
 }
+
+function colorChange(){
+    let userColor = prompt('Give me a color!')
+    extraMsg(userColor)
+    //document.write(userColor)
+    if (userColor == 'yellow') {
+        return  document.body.style.backgroundColor = userColor
+    //} else if ( typeof userColor === 'number') {
+        //return document.write(' , This color is extraordinary.')
+        
+    } else {
+        return document.body.style.backgroundColor = userColor}    
+    } 
+
